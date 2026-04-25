251f94c4b2839021bae187de879450d6f63dfb09
zeeshanmaqbool200 <zeeshanmaqbool200@gmail.com>
2026-04-25 19:24:52 +0530

    redefined UI fixed errors updated to version latest see changes in log

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+export const MAKTAB_LEVELS = [
+  'Awal', 'Doum', 'Soam', 'Chaharum', 'panjum', 'shahsum', 
+  'haftum', 'hashtum', 'dahum', 'Hafiz', 'muntazim [m]', 'muntazimah [f]'
+] as const;
+
+export const FEE_HEADS = [
+  'Monthly Fee', 'Admission Fee', 'Quran / Hifz Fee', 
+  'Exam / Test Fee', 'Book / Kitab Fee', 
+  'Activity / Competition Fee (Gez-z & Gen-x)', 
+  'Sadqa / Donation', 'Others'
+] as const;
+
+export const PAYMENT_MODES = [
+  'Cash', 'UPI', 'Bank Transfer', 'Card', 'Cheque', 'Others'
+] as const;
+
+export const SUBJECT_OPTIONS = [
+  'Quran Recitation', 'Diniyat', 'Urdu', 'Amali Masail', 
+  'Naat Khawni', 'Surahs Learning', 'Hifz', 
+  'Fiqh & Aqeedah', 'Gez-z / Gen-x Competitions'
+] as const;
